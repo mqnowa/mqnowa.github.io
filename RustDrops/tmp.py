48f5b91ab08e3cdd6c01e8203e32fdbe5deddbbb
Avent <abeckento@gmail.com>
2023-12-17 00:11:17 +0900

    make folders

---
+++ RustDrops/tmp.py
@@ -0,0 +1,12 @@
+with open("RustDrops.md") as f:
+    f.readline()
+    while True:
+        line = f.readline()
+        if not line:
+            break
+
+        if line.startswith("###"):
+            print(line[4:-1])
+        elif line.startswith("#"):
+            print(line[2:-1])
+        elif line.startswith("|"):
