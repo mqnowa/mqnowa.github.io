320158d145fb321922849d06f6117b23bf72ebf3
mqnowa <44436695+mqnowa@users.noreply.github.com>
2026-02-22 23:35:54 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mqnowa.github.io',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
